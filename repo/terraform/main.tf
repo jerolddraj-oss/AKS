87a937b8f1f0765631fc129aa0f91293b1e515f8
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-04-17 09:20:38 +0530

    Update main.tf

--- repo/terraform/main.tf
+++ repo/terraform/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "nodepool1"
     node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_B2s"
   }
 
   identity {
